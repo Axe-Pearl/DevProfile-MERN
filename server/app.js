fbcdc833b5c93b4bc919f8865a9d2b0245654ae7
Axe-Pearl <mycoursesbattletour@gmail.com>
2022-02-16 03:06:09 +0530

    storing data on mongodb online database

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT;
 
 require("./db/conn");
 app.use(express.json());
-// const User = require("./models/userSchema");
 app.use(require("./router/auth"));
 app.listen(PORT,(req,res)=>{
     console.log(`Server running on port ${PORT}`);
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+require("../db/conn");
+const User = require("../models/userSchema");
 
 router.get("/",(req,res)=>{
     res.send("Hello World! from router");
 });
 
-router.get("/register",(req,res)=>{
-    res.send("register");
-});
-
 router.post("/register",(req,res)=>{
-    console.log(req.body);
-    res.json({message: req.body});
+    const {name, email, phone, work, password, cpassword} = req.body;
+    if(!name || !email || !phone || !work || !password || !cpassword){
+        return res.status(422).json({error:"Please fill all required fields"});
+    }
+    User.findOne({email:email})
+    .then((userExist)=>{
+        if(userExist){
+            return res.status(422).json({message:"Email is already registered"});
+        }
+        
+        const user = new User({name, email, phone, work, password, cpassword});
+        user.save().then(()=>{
+            res.status(201).json({message:"user registered successfully!"})
+        })
+        .catch((err)=> res.status(500).json({error:"Failed to register"}))
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
 });
 module.exports = router;
